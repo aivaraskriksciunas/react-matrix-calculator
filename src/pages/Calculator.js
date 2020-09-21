f4097a09e1e7e5e113ac7f613e3382e89ee0463c
aivaraskriksciunas <aiviskri@gmail.com>
2020-09-21 21:24:18 +0300

    Calculator moved to separate component

---
+++ src/pages/Calculator.js
@@ -0,0 +1,137 @@
+import React from 'react';
+
+import MatrixGridController from '../components/Matrix/MatrixGridController'
+import SelectButton from '../components/SelectButton'
+import { createInitialMatrix } from '../components/Matrix/MatrixModifers'
+import MatrixOperations, {
+    OP_TRANSPOSE,
+    OP_DETERMINANT,
+    OP_SQUARE,
+    OP_CUBE,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY 
+} from '../components/Matrix/MatrixOperations'
+
+class Calculator extends React.Component {
+
+    constructor( props ) {
+        super( props )
+
+        this.state = {
+            matrixA: createInitialMatrix( 3, 3 ),
+            matrixB: createInitialMatrix( 3, 3 ),
+            resultMatrix: null,
+            calculationError: null,
+        }
+
+        this.createOpSelectHandler = this.createOpSelectHandler.bind( this )
+    }
+
+    swapMatrices() {
+        let matrixA = this.state.matrixA
+        this.setState({
+            matrixA: this.state.matrixB,
+            matrixB: matrixA
+        })
+    }
+
+    selectOperation( operation ) {
+        const [ result, error ] = MatrixOperations.doOperation( operation, this.state.matrixA, this.state.matrixB )
+
+        this.setState({
+            resultMatrix: result,
+            calculationError: error
+        })
+    }
+
+    createOpSelectHandler = ( operation ) => {
+        return () => this.selectOperation( operation )
+    }
+
+    render() {
+        return (
+            <div style={{ padding: '40px 0' }}>
+                <div className='container boxContainer'>
+                    <div className='sm:justify-center items-start  flex flex-wrap'>
+                        <div className='pr-8 mb-8' style={{ flex: 1 }}>
+                            <MatrixGridController 
+                                onChange={(mat) => this.setState({ matrixA: mat })} 
+                                matrix={this.state.matrixA}>
+                            </MatrixGridController>
+                        </div>
+                        <div className='mb-8' style={{ flex: 1 }}>
+                            <MatrixGridController 
+                                onChange={(mat) => this.setState({ matrixB: mat })} 
+                                matrix={this.state.matrixB}>
+                            </MatrixGridController>
+                        </div>
+                    </div>
+
+                    <ButtonRow>
+                        <SelectButton onSelect={this.swapMatrices.bind( this )}>Swap matrices</SelectButton>
+                    </ButtonRow>
+
+                    <ButtonRow title="Single matrix operations (using first matrix)">
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_TRANSPOSE )}>Transpose</SelectButton>
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_SQUARE )}>^ 2</SelectButton>
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_CUBE )}>^ 3</SelectButton>
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_DETERMINANT )}>Find determinant</SelectButton>
+                    </ButtonRow>
+
+                    <ButtonRow title="Arithmetic operations">
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_ADD )}>Add</SelectButton>
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_SUBTRACT )}>Subtract</SelectButton>
+                        <SelectButton onSelect={this.createOpSelectHandler( OP_MULTIPLY )}>Multiply</SelectButton>
+                    </ButtonRow>
+
+                </div>
+
+
+                <ResultContainer matrix={this.state.resultMatrix} error={this.state.calculationError}></ResultContainer>
+            </div>
+        )
+    }
+
+}
+
+function ButtonRow({ title, children }) {
+
+    return (
+        <>
+            {!!title ? <div className='font-lg mb-3'>{title}</div> : null}
+
+            <div className='flex flew-wrap mb-5'>
+                {children}
+            </div>
+        </>
+    )
+}
+
+function ResultContainer({ matrix, error }) {
+
+    if ( error ) {
+        return (
+            <div className='container boxContainer' style={{ borderTop: '5px solid #b71c1c'}}>
+                <div className='font-2xl mb-4'>Result</div>
+                {error}
+            </div>
+        )
+    }
+    else if ( matrix ) {
+        return (
+            <div className='container boxContainer' style={{ borderTop: '5px solid #009688'}}>
+                <div className='font-2xl mb-4'>Result</div>
+                <MatrixGridController 
+                    matrix={typeof( matrix ) == "number" ? [[ matrix ]] : matrix} 
+                    readonly>
+                </MatrixGridController>
+            </div>
+        )
+    }
+
+    return null;
+
+}
+
+export default Calculator;
